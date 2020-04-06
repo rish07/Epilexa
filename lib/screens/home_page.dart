2009d31a84ce8ef0af4197b8ca5167e0c0c7c1c1
Creator <rishirajsinghchhabra@gmail.com>
2020-04-06 18:25:23 +0530

    Appbar complete

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:epilexa/fonts/epilexa_icons.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -8,6 +9,29 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'EPILEXA',
+          style: TextStyle(
+              color: Color(0xFF0075FF),
+              fontWeight: FontWeight.bold,
+              fontSize: 30),
+        ),
+        centerTitle: true,
+        backgroundColor: Color(0xFFEAEAEA),
+        elevation: 0,
+        actions: <Widget>[
+          IconButton(
+              icon: Icon(
+                Epilexa.vectorprofile,
+                color: Colors.black,
+                size: 20,
+              ),
+              onPressed: (){})
+        ],
+      ),
+      backgroundColor: Color(0xFFEAEAEA),
+    );
   }
 }
